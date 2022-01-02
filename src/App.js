5e3abf8e3a1c742cf98a84cbe3e89b112a6993dd
gustavohdev <g_a93@outlook.com>
2022-01-02 23:11:13 +0000

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import AddProduct from "./components/AddProduct";
+import ProductsList from "./components/ProductsList.js";
+import SingleProduct from "./components/SingleProduct.js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <aside>
+                    <Link to={`/`}>Products</Link>
+                    <Link to={`/add-product`}>Add product</Link>
+                </aside>
+
+                <main>
+                    <Routes>
+                        <Route exact path="/" component={ProductsList} />
+                        <Route path="/add-product" component={AddProduct} />
+                        <Route path="/product/:slug" component={SingleProduct} />
+                    </Routes>
+                </main>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
